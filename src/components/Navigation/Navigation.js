a84e837393b1d51e12c3974ff53c3c91cbe1c518
DevonGifford <DevonGifford@outlook.com>
2023-07-17 18:41:46 +0200

    fixing routing issue in prod

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,56 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = ({ onRouteChange, isSignedIn }) => {
     if (isSignedIn) {
       return (
-        <nav style={{display: 'flex',  justifyContent: 'space-between', padding: '2px 20px 2px', zIndex: '1'}}>
 
+        <nav style={{ display: 'flex', justifyContent: 'space-between', padding: '2px 20px 2px', zIndex: '1' }}>
+          
+          
           {/* LEFT HALF */}
           <div className='flex items-start'>
-            <p 
-              onClick={() => onRouteChange('about')} 
-              className='f3 link dim white underline pa3 pointer' 
-              id='signin'
-            >
+            <Link to="/about" className='f3 link dim white underline pa3 pointer' id='signin'>
               About
-            </p>
+            </Link>
           </div>
-
-
+          
           {/* RIGHT HALF */}
-          <div className='flex items-end '>
-            <p 
-              onClick={() => onRouteChange('signout')} 
-              className='f3 link dim white underline pa3 pointer' 
-              id='signin'
-            >
+          <div className='flex items-end'>
+            <p onClick={() => onRouteChange('signout')} className='f3 link dim white underline pa3 pointer' id='signin'>
               Sign Out
             </p>
           </div>
+
+
         </nav>
       );
     } else {
       return (
-        <nav style={{display: 'flex', justifyContent: 'flex-end', padding: '2px 20px 2px'}}>
+        <nav style={{ display: 'flex', justifyContent: 'flex-end', padding: '2px 20px 2px' }}>
 
-          <p 
-            onClick={() => onRouteChange('signin')} 
-            className='f3 link dim white underline pa3 pointer' 
-            id='signin'
-          >
+          <Link to="/" className='f3 link dim white underline pa3 pointer' id='signin'>
             Sign In
-          </p>
-          
-          
-          <p 
-            onClick={() => onRouteChange('register')} 
-            className='f3 link dim white underline pa3 pointer' 
-            id='register'
-          >
-            Register
-          </p>
+          </Link>
 
-          
+          <Link to="/register" className='f3 link dim white underline pa3 pointer' id='register'>
+            Register
+          </Link>
+
         </nav>
       );
     }
